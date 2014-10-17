3fd4f40f4626c989e1d3c7f2a4698d51f7408176
Alexander Brown <alexander.brown@ucl.ac.uk>
2014-10-17 12:42:12 +0100

    quickOpen now defaults to quick method, and allows full file opening by omitting regionSpec

--- quickOpenStitchedImageRegion.m
+++ quickOpenStitchedImageRegion.m
@@ -10,33 +10,50 @@ elseif ~exist(fileName, 'file')
     error('File specified does not exist')
 end
 
-if any(regionSpec)<1
-    error('All elements of regionSpec must be >0.\n Start indices are 1-based and w and h must be positive integers')
-end
-%%
 
-x=regionSpec(1);
-y=regionSpec(2);
-w=regionSpec(3);
-h=regionSpec(4);
-y2=y+h-1;
-x2=x+w-1;
+% Default to fastest method
+if nargin<3
+    methodFlag=2;
+end
+% Crop spec
+
+if nargin>1
+    if ~isnumeric(regionSpec)||~isvector(regionSpec)||numel(regionSpec)~=4||any(regionSpec<1)
+            error(sprintf('regionSpec must be 4-element numeric vector with all elements > 0.\n Start indices are 1-based; w and h must be positive integers'))
+    end
+    [x,y,~,h,x2,y2]=getCropParams(regionSpec);
+    doCrop=1;
+else
+    doCrop=0;
+end
 
 %% Do it
 switch methodFlag
     case 0 %Read in, select region in memory. ~6s per slice
         I=imread(fileName);
-        I=I(y:y2, x:x2);
+        if doCrop
+            I=I(y:y2, x:x2);
+        end
     case 1 % Pixel region selection in imread. ~6s per slice
-        I=imread(fileName, 'PixelRegion', {[y y2] [x x2]});
+        if doCrop
+            I=imread(fileName, 'PixelRegion', {[y y2] [x x2]});
+        else
+            error('Method 1 (PixelRegion) requires a specified region. To use this option with no crop, choose method 0')
+        end
     case 2 % fread needed lines only; select pixels within line in memory. This is the fastest currently implemented.
         fh=fopen(fileName);
         info=imfinfo(fileName);
+        if ~doCrop
+            [x,y,~,h,x2,~]=getCropParams([1 1 info.Width info.Height]);
+        end
         fseek(fh, info.StripOffsets+(y-1)*info.Width*info.BitDepth/8, 'bof');
         I=fread(fh, [info.Width, h], ['*' selectMATLABDataType(info)], 0, 'ieee-be');
         I=I(x:x2, :)';
     case 3 %memmap method
         info=imfinfo(fileName);
+        if ~doCrop
+            [x,y,~,~,x2,y2]=getCropParams([1 1 info.Width info.Height]);
+        end
         m=memmapfile(fileName, 'Offset', info.StripOffsets, 'Format', {selectMATLABDataType(info), [info.Width info.Height], 'm'});
         dat=m.Data;
         I=dat.m(x:x2, y:y2);
@@ -44,6 +61,17 @@ switch methodFlag
 end
 
 end
+
+
+function [x,y,w,h,x2,y2]=getCropParams(regionSpec)
+   x=regionSpec(1);
+    y=regionSpec(2);
+    w=regionSpec(3);
+    h=regionSpec(4);
+    y2=y+h-1;
+    x2=x+w-1;
+end
+
 
 function dt=selectMATLABDataType(info)
 switch info.BitDepth
