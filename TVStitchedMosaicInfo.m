284de6213318425cbf3747c3a64443255d2ca9cc
Alexander Brown <alexander.brown@ucl.ac.uk>
2014-10-17 11:53:09 +0100

    Minor refactor to mosaicInfo class

--- TVStitchedMosaicInfo.m
+++ TVStitchedMosaicInfo.m
@@ -36,6 +36,8 @@ end
 
 function obj=getMosaicMetaData(obj)
 
+delimiterInTextFile='\r\n';
+
 %% Get matching files
 metaDataFileName=dir(fullfile(obj.baseDirectory,'Mosaic*.txt'));
 if isempty(metaDataFileName)
@@ -49,7 +51,7 @@ metaDataFullPath=fullfile(obj.baseDirectory, metaDataFileName.name);
 %% Open
 fh=fopen(metaDataFullPath);
 %% Read
-txtFileContents=textscan(fh, '%s', 'Delimiter', '\r\n');
+txtFileContents=textscan(fh, '%s', 'Delimiter', delimiterInTextFile);
 txtFileContents=txtFileContents{1};
 %% Parse
 info=struct;
@@ -80,6 +82,8 @@ end
 
 function obj=getStitchedImagePaths(obj)
 
+delimiterInTextFile='\r\n';
+
 searchPattern=[obj.sampleName, '_StitchedImagesPaths_'];
 
 listFilePaths=dir(fullfile(obj.baseDirectory, [searchPattern '*.txt']));
@@ -93,7 +97,7 @@ for ii=1:numel(listFilePaths)
     fp=fullfile(obj.baseDirectory, listFilePaths(ii).name);
     fh=fopen(fp);
     %% Read in file paths
-    channelFilePaths=textscan(fh, '%s', 'Delimiter', '\r\n');
+    channelFilePaths=textscan(fh, '%s', 'Delimiter', delimiterInTextFile);
     %% Close
     fclose(fh);
     %% Get channel name
