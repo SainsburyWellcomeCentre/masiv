c9a9b3004797f7e0d9011f997036fce1e3453062
Rob Campbell <git@raacampbell.com>
2015-03-30 20:01:25 +0200

    On a multi-user system we run into permissions problems if two people are using goggleViewer at the same time since the readQueue file in /tmp/ always has the same name. This change creates a uniquely named readQueue file each time.

--- resources/readQueueSize/readQueueFileFullPath.m
+++ resources/readQueueSize/readQueueFileFullPath.m
@@ -1,6 +1,15 @@
 function fName = readQueueFileFullPath
+% function fName = readQueueFileFullPath
+%
+% The name of the read queue file. On a multi-user system
+% we run into problems if two people are using goggleViewer
+% at the same time. Thus we append the date and time to the
+% file name to create a unique file each time. 
 
-fName=fullfile(tempdir, 'readqueuesz.txt');
+  fName=fullfile(tempdir,...
+                 sprintf('goggleViewer_readqueue_%s.txt',...
+                         datestr(now,'yymmdd_HHMMSS')) );
+
 
 end
 
