a3aa9e6b9d81d4737a689b937613a2fc07e18735
Rob Campbell <git@raacampbell.com>
2015-03-30 16:30:28 +0200

    Identify which file causes a failure with addLineToReadQueueFile

--- resources/readQueueSize/addLineToReadQueueFile.m
+++ resources/readQueueSize/addLineToReadQueueFile.m
@@ -1,5 +1,12 @@
 function addLineToReadQueueFile
-    fid=fopen(readQueueFileFullPath, 'a');
-    fprintf(fid, '\n');
-    fclose(fid);
+	try
+	    fid=fopen(readQueueFileFullPath, 'a');
+	    fprintf(fid, '\n');
+	    fclose(fid);
+	catch
+		fprintf('%s: failed with file name: %s\n',mfilename,readQueueFileFullPath)
+		rethrow(lasterror)
+	end
+
+
 end
