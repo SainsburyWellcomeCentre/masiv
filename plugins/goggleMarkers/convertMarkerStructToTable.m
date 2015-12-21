d36f7884b54eb2fa13aec3641443f61476dd3684
Alexander Brown <alexander.brown@ucl.ac.uk>
2015-12-21 10:23:52 +0000

    Update to goggleMarker conversion to table

--- plugins/goggleMarkers/convertMarkerStructToTable.m
+++ plugins/goggleMarkers/convertMarkerStructToTable.m
@@ -7,9 +7,9 @@ function T=convertMarkerStructToTable(yml) %#ok<*AGROW>
 %  Y=readSimpleYAML('my_points.yml')
 %  T=convertMarkerStructToTable(T)
 
-x=[];
-y=[];
-z=[];
+ML=[];
+DV=[];
+PA=[];
 type={};
 f=fieldnames(yml);
 for ii=1:numel(f)
@@ -18,13 +18,13 @@ for ii=1:numel(f)
         thisTypeY=[yml.(f{ii}).markers.y]';
         thisTypeZ=[yml.(f{ii}).markers.z]';
         
-        x=[x; thisTypeX];
-        y=[y; thisTypeY];
-        z=[z; thisTypeZ];
+        ML=[ML; thisTypeX];
+        DV=[DV; thisTypeY];
+        PA=[PA; thisTypeZ];
         type=[type; repmat(f(ii), numel(thisTypeX), 1)];
     end
 end
 
 type=categorical(type);
-T=table(x, y, z, type);
+T=table(ML, DV, PA, type);
 end
