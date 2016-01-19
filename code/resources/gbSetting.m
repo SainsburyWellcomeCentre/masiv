0a96ba7020af7c42c25624e39331c082327866bb
Rob Campbell <git@raacampbell.com>
2016-01-19 11:07:38 +0100

    neaten settings file

--- code/resources/gbSetting.m
+++ code/resources/gbSetting.m
@@ -1,5 +1,10 @@
 function valOut=gbSetting(nm, val) %#ok<INUSD>
-%GBSETTING Gets or sets settings from a YAML prefs file
+%gbSetting 
+% Gets or sets settings (preferences) for goggleViewer from a YAML prefs file.
+% gbSetting also stores the default preferences and creates the YAML prefs 
+% file if it's missing. 
+
+
 persistent r fName fileInfo
 
 if isempty(r)
@@ -49,23 +54,25 @@ end
 function createDefaultPrefsFile()
     %% Global settings
     s.defaultDirectory='/';
-    
     s.font.name='Helvetica';
     s.font.size=12;
     
     %% Default voxel sizes
     s.defaultVoxelSize=[1 1 5];
-    %% Main Viewer
+
+    %% Main Viewer colors
     s.viewer.panelBkgdColor=[0.1 0.1 0.1];
     s.viewer.mainBkgdColor=[0.2 0.2 0.2];
     s.viewer.textMainColor=[0.8 0.8 0.8];
     s.viewer.mainFigurePosition=[0 0 1600 900];
         
+    %% Main viewer navigation settings
     s.navigation.panIncrement=[10 120]; % shift and non shift; fraction to move view by
     s.navigation.scrollIncrement=[10 1]; %shift and non shift; number of images to move view by
     s.navigation.zoomRate=1.5;
     s.navigation.panModeInvert=0;
     s.navigation.keyboardUpdatePeriod=0.02; %20ms keyboard polling
+
     %% GVD Settings
     s.viewerDisplay.nPixelsWidthForZoomedView=2000;
     s.viewerDisplay.minZoomLevelForDetailedLoad=1.5;
@@ -85,7 +92,6 @@ function createDefaultPrefsFile()
     s.debug.outputSpacing=12;
 %%
     baseDir=fileparts(which('goggleViewer'));
-    
     writeSimpleYAML(s, fullfile(baseDir, 'gogglePrefs.yml'));
     
 end
