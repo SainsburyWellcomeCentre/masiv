e07f1a6af1def672ef711b25d74131ab3d8b99d1
Rob Campbell <git@raacampbell.com>
2016-01-19 19:00:34 +0100

    format some lines and print simple instructions to screen when program starts

--- code/classes/TVStitchedMosaicInfo.m
+++ code/classes/TVStitchedMosaicInfo.m
@@ -23,10 +23,11 @@ classdef TVStitchedMosaicInfo
             elseif ~exist(baseDirectory, 'dir')
                 error('Specified path does not exist')
             end
+
             %% Get file parts
             [~, obj.experimentName]=fileparts(baseDirectory);
             obj.baseDirectory=baseDirectory;
-            
+
             %% Get metadata
             obj=getMosaicMetaData(obj);
             obj.sampleName=obj.metaData.SampleID;
@@ -34,10 +35,16 @@ classdef TVStitchedMosaicInfo
             obj=getStitchedImagePaths(obj);
             %% Get available downscaled stacks
         end
+
         function ds=get.downscaledStacks(obj)
             pathToDSObjsFile=fullfile(obj.baseDirectory, [obj.sampleName '_GBStacks'], [obj.sampleName '_GBStackInfo.mat']);
             if ~exist(pathToDSObjsFile, 'file')
                 ds=[];
+                fprintf('\n\n\t=====>  Directory %s contains no down-scaled image stacks  <=====\n',obj.baseDirectory)
+                fprintf('\n\n\t\tINSTRUCTIONS')
+                fprintf('\n\n\tYou will need to generate down-scaled image stacks in order to proceed.')
+                fprintf('\n\tClick "New", then the channel you want to build, then the section range.')
+                fprintf('\n\tSee documention on the web for more information.\n\n\n')
             else
                 a=load(pathToDSObjsFile);
                 ds=a.stacks;
@@ -46,6 +53,7 @@ classdef TVStitchedMosaicInfo
                 end
             end
         end
+
         function dsl=get.downscaledStackList(obj)
             ds=obj.downscaledStacks;
             if ~isempty(ds)
@@ -55,6 +63,7 @@ classdef TVStitchedMosaicInfo
             end
             dsl=dsl(:);
         end
+
         function obj=changeImagePaths(obj, strToReplace, newStr)
                 % Allows batch editing of the image paths. This can be
                 % needed if the base directory is changed
@@ -70,84 +79,87 @@ classdef TVStitchedMosaicInfo
 end
 
 function obj=getMosaicMetaData(obj)
+    %Get meta-data from TissueVision Mosaic file
+    delimiterInTextFile='\r\n';
 
-delimiterInTextFile='\r\n';
-
-%% Get matching files
-metaDataFileName=dir(fullfile(obj.baseDirectory,'Mosaic*.txt'));
-if isempty(metaDataFileName)
-    error('Mosaic metadata file not found')
-elseif numel(metaDataFileName)>1
-    error('Multiple metadata files found. There should only be one matching ''Mosaic*.txt''')
-end
-
-metaDataFullPath=fullfile(obj.baseDirectory, metaDataFileName.name);
-
-%% Open
-fh=fopen(metaDataFullPath);
-%% Read
-txtFileContents=textscan(fh, '%s', 'Delimiter', delimiterInTextFile);
-txtFileContents=txtFileContents{1};
-%% Parse
-info=struct;
-for ii=1:length(txtFileContents)
-    spl=strsplit(txtFileContents{ii}, ':');
-    
-    if numel(spl)<2
-        error('Invalid name/value pair: %s', txtFileContents{ii})
-    elseif numel(spl)>2
-        spl{2}=strjoin(spl(2:end), ':');
-        spl=spl(1:2);
+    %% Get matching files
+    metaDataFileName=dir(fullfile(obj.baseDirectory,'Mosaic*.txt'));
+    if isempty(metaDataFileName)
+        error('Mosaic metadata file not found')
+    elseif numel(metaDataFileName)>1
+        error('Multiple metadata files found. There should only be one matching ''Mosaic*.txt''')
     end
-    nm=strrep(spl{1}, ' ', '');
-    val=spl{2};
-    valNum=str2double(val);
-    if ~isempty(valNum)&&~isnan(valNum)
-        val=valNum;
+
+    metaDataFullPath=fullfile(obj.baseDirectory, metaDataFileName.name);
+
+    %% Open
+    fh=fopen(metaDataFullPath);
+    
+    %% Read
+    txtFileContents=textscan(fh, '%s', 'Delimiter', delimiterInTextFile);
+    txtFileContents=txtFileContents{1};
+    
+    %% Parse
+    info=struct;
+    for ii=1:length(txtFileContents)
+        spl=strsplit(txtFileContents{ii}, ':');
+    
+        if numel(spl)<2
+            error('Invalid name/value pair: %s', txtFileContents{ii})
+        elseif numel(spl)>2
+            spl{2}=strjoin(spl(2:end), ':');
+            spl=spl(1:2);
+        end
+        nm=strrep(spl{1}, ' ', '');
+        val=spl{2};
+        valNum=str2double(val);
+        if ~isempty(valNum)&&~isnan(valNum)
+            val=valNum;
+        end
+    
+        info.(nm)=val;
     end
-    
-    info.(nm)=val;
-    
-end
-%% Close
-fclose(fh);
-%% Assign
-if isempty(info)||~isstruct(info)
-    error('Invalid metadata file')
-else
-    obj.metaData=info;
-end
-end
+
+    fclose(fh); 
+
+    %% Assign
+    if isempty(info)||~isstruct(info)
+        error('Invalid metadata file')
+    else
+        obj.metaData=info;
+    end
+end %function obj=getMosaicMetaData(obj)
+
+
 
 function obj=getStitchedImagePaths(obj)
+    %Get paths to stitched (full-resolution) images from text files
+    delimiterInTextFile='\r\n';
+    searchPattern=[obj.sampleName, '_StitchedImagesPaths_'];
+    listFilePaths=dir(fullfile(obj.baseDirectory, [searchPattern '*.txt']));
+    if isempty(listFilePaths)
+        fprintf('\n\n\t*****\n\tCan not find text files listing the relative paths to the full resolution images.\n\tYou need to create these text files. Please see the documentation on the web.\n\tQUITING.\n\t*****\n\n\n')
+        error('No %s*.txt files found.\n',searchPattern)
+    end
 
-delimiterInTextFile='\r\n';
+    obj.stitchedImagePaths=struct;
 
-searchPattern=[obj.sampleName, '_StitchedImagesPaths_'];
-
-listFilePaths=dir(fullfile(obj.baseDirectory, [searchPattern '*.txt']));
-if isempty(listFilePaths)
-  error('No %s*.txt files found.\n',searchPattern)
-end
-
-obj.stitchedImagePaths=struct;
-
-
-for ii=1:numel(listFilePaths)
-    %% Open txt file
-    fp=fullfile(obj.baseDirectory, listFilePaths(ii).name);
-    fh=fopen(fp);
-    %% Read in file paths
-    channelFilePaths=textscan(fh, '%s', 'Delimiter', delimiterInTextFile);
-    %% Close
-    fclose(fh);
+    for ii=1:numel(listFilePaths)
+        %% Open txt file
+        fp=fullfile(obj.baseDirectory, listFilePaths(ii).name);
+        fh=fopen(fp);
     
-    %% strip out absolute path to get relative file paths
-    channelFilePaths=strrep(channelFilePaths{1}, [obj.baseDirectory,filesep], '');
-    %% Get channel name
-    channelName=strrep(strrep(listFilePaths(ii).name, searchPattern, ''), '.txt', '');
-    %% Assign
-    obj.stitchedImagePaths.(channelName)=channelFilePaths;
-end
-end
+        %% Read in file paths
+        channelFilePaths=textscan(fh, '%s', 'Delimiter', delimiterInTextFile);
+        fclose(fh);
+    
+        %% strip out absolute path to get relative file paths
+        channelFilePaths=strrep(channelFilePaths{1}, [obj.baseDirectory,filesep], '');
 
+        %% Get channel name
+        channelName=strrep(strrep(listFilePaths(ii).name, searchPattern, ''), '.txt', '');
+
+        %% Assign
+        obj.stitchedImagePaths.(channelName)=channelFilePaths;
+    end
+end %function obj=getStitchedImagePaths(obj)
