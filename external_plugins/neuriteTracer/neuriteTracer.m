8fc3721ae1b6e0e25db0d441b8971eb521274216
Rob Campbell <git@raacampbell.com>
2016-03-02 16:10:38 +0100

    rename plugin directories to remove the underscore

--- external_plugins/neuriteTracer/neuriteTracer.m
+++ external_plugins/neuriteTracer/neuriteTracer.m
@@ -108,6 +108,12 @@ classdef neuriteTracer<masivPlugin
                 fprintf('\n\n\tThe matlab-tree package is not installed. \n\tPlease install from: https://github.com/raacampbell13/matlab-tree\n\n\n')
                 return
             end
+
+            %Ensure the helper functions are added to the path
+            pathToAdd = masiv_plugin.absolutepath('neuriteTracerFunctions');
+            addpath(pathToAdd) %Duplicates aren't added
+
+
             obj=obj@masivPlugin(caller); %call constructor of masivPlugin
             obj.MaSIV=caller.UserData;
 
